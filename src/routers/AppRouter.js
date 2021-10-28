b132f2307a8527fd5745620ce27600ea09c3975b
Paulo Vergara <paulovmnzzi@gmail.com>
2021-10-28 09:48:02 -0300

    change browser for hash

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,5 +1,5 @@
 import React, { useContext } from 'react'
-import { BrowserRouter, Switch } from 'react-router-dom'
+import { HashRouter, Switch } from 'react-router-dom'
 import AuthContext from '../auth/AuthContext'
 import LoginScreen from '../components/login/LoginScreen'
 import DashboardRoute from './DashboardRoute'
@@ -13,14 +13,14 @@ const AppRouter = () => {
 
     return (
 
-        <BrowserRouter>
+        <HashRouter>
             <div>
                 <Switch>
                     <PublicRoute exact path='/login' component={LoginScreen}  isAuthenticated={logged} />
                     <PrivateRoute path='/' component={DashboardRoute} isAuthenticated={logged} />
                 </Switch>
             </div>
-        </BrowserRouter>
+        </HashRouter>
 
     )
 }
